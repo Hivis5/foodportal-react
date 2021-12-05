56aa152df3e4790ca3fa7c17ab27322aabcbe970
unknown <hiteshvishalsubramani@gmail.com>
2021-12-05 13:01:46 +0530

    React Food Portal

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React,{useContext} from 'react'
+import {FoodContext} from '../App';
+import {Link} from 'react-router-dom'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+function Header() {
+
+  let context  = useContext(FoodContext)
+  console.log(context)
+
+  return (
+    <div className = "head-wrapper">
+      <div className ="head-title">
+         Food Ordering Portal
+      </div>
+      <div className="head-cart">
+        <Link to ='cart'>< ShoppingCartIcon /></Link>
+        <span className="count"> {context.cartValue}</span>
+      </div>     
+    </div>
+  )
+}
+
+export default Header
